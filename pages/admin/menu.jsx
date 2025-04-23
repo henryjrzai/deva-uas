d59ff2a765cd70171684bfa808a36d586cfac537
AlvharetVandeva <vandevlomboe@gmail.com>
2025-04-23 09:44:17 +0700

    menghapus komponent yang tidak ada

--- pages/admin/menu.jsx
+++ pages/admin/menu.jsx
@@ -1,12 +1,7 @@
-import Sidebar from '../../components/Layout/Sidebar';
-import ProductCRUD from '@pages/components/Dashboard/ProductCRUD';
-
 export default function AdminMenu() {
   return (
     <div className="flex min-h-screen">
-      <Sidebar />
       <main className="flex-grow p-6">
-        <ProductCRUD />
       </main>
     </div>
   );
